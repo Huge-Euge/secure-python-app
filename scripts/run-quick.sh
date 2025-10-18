805915dda7aa3e2a11f120ec4e56f6a1f456a38c
ken <eugenekennyjensen@gmail.com>
2025-10-18 13:28:18 -0400

    Updated scripts

---
+++ scripts/run-quick.sh
@@ -0,0 +1,22 @@
+# Exit if any command fails
+set -e
+
+cleanup() {
+    echo "--- Shutting down containers ---"
+    sudo docker compose down
+}
+
+# make cleanup function get called on script exit
+trap cleanup ERR INT TERM
+
+echo "--- Building Docker image ---"
+# 'build' uses your Dockerfile and docker-compose.yml to build the image
+sudo docker compose build
+
+echo "--- starting container in background ---"
+# up -d starts it detached 
+sudo docker compose up -d
+
+echo "---"
+echo "Now running in the background"
+echo "To stop it, run: sudo docker compose down"
