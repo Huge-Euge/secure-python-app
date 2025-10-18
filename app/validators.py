8c0c03fd67c56085264d5d79300bea8ac83d7889
ken <eugenekennyjensen@gmail.com>
2025-10-17 22:46:44 -0400

    Refactored validators to use results types

--- app/validators.py
+++ app/validators.py
@@ -1,47 +1,104 @@
-from typing import Union, NamedTuple
+"""
+A module implementing form validation for the forms of the website
+"""
+
+import re
+from results import ManyResults, Result, merge_many_results
+from dal import Sqlite3DAL
 
 
-class Error(NamedTuple):
-    """
-    Error message wrapper for typed union
-    """
-
-    value: str
-
-
-ValidationResult = Union[True, Error]
-
-
-def validate_registration(form, db_):
+def validate_registration(form, db_) -> ManyResults:
     """
     Validates the user registration form.
-    Returns an Error(str) failure, or True on success.
+    Returns an Error(str) on failure, or True on success.
     """
     username = form.get("username")
     password = form.get("password")
     password_2 = form.get("password_2")
+    results = Result.Success()
 
-    # Check for empty fields
-    if not username or not password or not password_2:
-        return Error("Username and password are required.")
+    username_validation = is_valid_username(username)
+    results = merge_many_results(results, username_validation)
+
+    password_validation = is_valid_password(password, password_2)
+    results = merge_many_results(results, password_validation)
+
+    # Check if the username is already taken
+    user = Sqlite3DAL.find_user_by_username(db_, username)
+    if isinstance(user, Result.Success):
+        results = merge_many_results(
+            results,
+            [Result.Error("That username is already taken. Please choose another.")],
+        )
+
+    return results
+
+
+def is_valid_username(username: str) -> ManyResults:
+    """
+    Check if a username contains only whitelisted characters.
+    (a-z, A-Z, 0-9, underscore, hyphen)
+    """
+    results = Result.Success()
+
+    if not username.strip():
+        results = merge_many_results(
+            results, [Result.Error("Username is required cannot contain whitespace.")]
+        )
 
     # Enforce length constraints
     if len(username) < 5 or len(username) > 25:
-        return Error("Username must be between 5 and 25 characters.")
+        results = merge_many_results(
+            results, [Result.Error("Username must be between 5 and 30 characters.")]
+        )
+
+    if not re.match(r"^[a-zA-Z0-9_-]{4,30}$", username):
+        results = merge_many_results(
+            results,
+            [
+                Result.Error(
+                    """
+            Username must be 5-30 characters long and contain only letters,
+            numbers, underscores or hyphens.
+            """
+                )
+            ],
+        )
+
+    return results
+
+
+def is_valid_password(password: str, password_2: str) -> ManyResults:
+    """
+    Check if a password is valid.
+    """
+    results = Result.Success()
+
+    if not password or not password_2:
+        results = merge_many_results(
+            results, [Result.Error("Please enter your password twice.")]
+        )
 
     if len(password) < 8:
-        return Error("Password must be at least 8 characters long.")
+        results = merge_many_results(
+            results, [Result.Error("Password must be at least 8 characters long.")]
+        )
 
     if password != password_2:
-        return Error("Passwords do not match.")
+        results = merge_many_results(results, [Result.Error("Passwords do not match.")])
 
-    # Check if the username is already taken
-    user = db_.execute(
-        "SELECT username FROM users WHERE username = ?", (username,)
-    ).fetchone()
+    return results
 
-    if user:
-        return "That username is already taken. Please choose another."
 
-    # If all checks pass, return True
-    return True
+def validate_note(form) -> Result.Type:
+    """
+    Validates an attempt to edit or create a note.
+    Returns an Error(str) on failure, or True on success.
+    """
+
+    content: str = form.get("note_content")
+
+    if not content.strip():
+        return Result.Error("Note content cannot be empty.")
+
+    return Result.Success()
