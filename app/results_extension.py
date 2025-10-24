f4b3e17f663617599f98563a1fa47b751597aa37
ken <eugenekennyjensen@gmail.com>
2025-10-24 14:39:49 -0400

    Comment and reword results_extension.py

--- app/results_extension.py
+++ app/results_extension.py
@@ -1,5 +1,5 @@
 """
-Utility functions to help concatenate Results
+Utility functions to help handle Results
 """
 
 from returns.result import Result, Success, Failure
@@ -8,6 +8,10 @@ from returns.result import Result, Success, Failure
 def merge_results(
     a: Result[None, list[str]], b: Result[None, list[str]]
 ) -> Result[None, list[str]]:
+    """
+    This helper function merges Failures that are lists of strs
+    with Success(None)'s
+    """
     if isinstance(a, Success) and isinstance(b, Success):
         return a
     if isinstance(a, Failure) and isinstance(b, Failure):
